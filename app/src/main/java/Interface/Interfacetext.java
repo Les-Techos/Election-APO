3ee9cae27c231ebbc255f0ebc3dd59e53167c8a7
Fares <fzraptor5@gmail.com>
2022-01-23 14:27:41 +0100

    Fares Simulation et interface textuelle

--- app/src/main/java/Interface/Interfacetext.java
+++ app/src/main/java/Interface/Interfacetext.java
@@ -1,5 +1,9 @@
 package Interface;
+import java.util.*;  
+import java.util.Scanner;
 
 public class Interfacetext {
-    
+    public Interfacetext() {
+        
+    }
 }
--- app/src/main/java/Simulation/Simulation.java
+++ app/src/main/java/Simulation/Simulation.java
@@ -1,16 +1,20 @@
 package Simulation;
 import Personne.*;
+import Scrutin.*;
+import utils.SaveManager;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 
 public class Simulation {
-    private ArrayList<Candidat> c;
-    private ArrayList<Electeur> e;
-
+    private HashSet<Candidat> c;
+    private HashSet<Electeur> e;
+    private Scrutin sa;
+    
     public Simulation(){
-        this.c = new ArrayList<Candidat>();
-        this.e = new ArrayList<Electeur>();
-        ArrayList<Electeur> e = new ArrayList<Electeur>();
+        this.c = new HashSet<Candidat>();
+        this.e = new HashSet<Electeur>();
+        HashSet<Electeur> e = new HashSet<Electeur>();
 
         for(int i = 0; i < 10; i++){
             try{
@@ -23,10 +27,45 @@ public class Simulation {
         }
         
         System.out.println("Liste candidat"+c);
-        System.out.println("Liste Electeur"+c);
+        System.out.println("Liste Electeur"+e);
     }
-    public Simulation(ArrayList<Candidat> c,ArrayList<Electeur> e){
-
+    public Simulation(HashSet<Candidat> c,HashSet<Electeur> e){
+        this.c = c;
+        this.e = e;
     }
     
+    // choix du mode scrutin
+    public void choixScrutin(int choix){    
+        switch (choix){
+            case 0:
+                sa = new scr_Majoritaire_1tour(this.e,this.c);
+                break;
+            case 1:
+                sa = new scr_Majoritaire_2tour(this.e,this.c);
+                break;
+            case 2:
+                sa = new scr_Alternatif(this.e,this.c);
+                break;
+            case 3:
+                sa = new scr_Approbation(this.e,this.c);
+                break;
+            case 4:
+                sa = new scr_Borda(this.e,this.c);
+                break;   
+        }  
+    }
+
+    public void interraction_social(){
+        
+    }
+
+    public ArrayList<Candidat> Lancerelection(){ 
+        ArrayList<Candidat> res = null;
+        try{
+            res = sa.getClassementCandidat();   
+        }catch(Exception e){
+            System.out.println("Scrutin  non choisie");
+        }
+        return res;
+    }
 }
