04134aa920b5021901f5c0689b831c2571623400
Fares <fzraptor5@gmail.com>
2022-01-25 14:38:52 +0100

    interface graphique premier menu et panel

--- app/src/main/java/Interface/Interfacegraph.java
+++ app/src/main/java/Interface/Interfacegraph.java
@@ -1,39 +1,29 @@
 package Interface;
 
 import java.awt.LayoutManager;
-
+import java.awt.BorderLayout;
+import java.awt.*;
 import javax.swing.*;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JPanel;
 
 import javafx.scene.paint.Color;
 
 public class Interfacegraph extends JFrame {
     public Interfacegraph() {
-        super();
-        build();
-    }
-
-    private void  build() {
         setTitle("ELECTION-TIM-FARES");
 		setSize(1200,800);
 		setResizable(false);
 		setLocationRelativeTo(null);
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setContentPane(buildContentPane());
-    }
-    private JPanel buildContentPane() {
-        JPanel panel = new JPanel();
-		
+        setLayout(new BorderLayout());
+        JPanel menu = new JPanel();
+        this.add(menu, BorderLayout.SOUTH);
 		JLabel label = new JLabel("Résultat: pas encore calculé ");
 		JButton bouton = new JButton("Calculer");
 		JTextField textField = new JTextField("écrire la");
 		textField.setColumns(10);
-		panel.add(textField);
-		panel.add(bouton);
-		panel.add(label);
-	 
-		return panel;
+        
+		menu.add(textField);
+		menu.add(bouton);
+		menu.add(label);
+        setDefaultCloseOperation(DISPOSE_ON_CLOSE);
     }
 }
