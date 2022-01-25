ff2bc00f3add92477ec2e9fe84477d4a90cab061
Fares <fzraptor5@gmail.com>
2022-01-25 23:24:32 +0100

    Fares

--- app/src/main/java/Interface/Interfacegraph.java
+++ app/src/main/java/Interface/Interfacegraph.java
@@ -22,7 +22,7 @@ public class Interfacegraph extends JFrame {
 
         // initialistaion de la FRAME
         setTitle("ELECTION-TIM-FARES");
-        setSize(1200, 800);
+        setSize(1280, 1024);
         setResizable(false);
         setLocationRelativeTo(null);
         
@@ -40,32 +40,38 @@ public class Interfacegraph extends JFrame {
         
 
         // création composant
-        JLabel label = new JLabel("Listes des Candidats");
+        JLabel label_list1 = new JLabel("Listes des Candidats");
+        JLabel label_list2 = new JLabel("Resultat des Elections");
         JButton btn_demarer_data = new JButton("Simulation depuis un fichier");
         JButton btn_demarer_rand = new JButton("Simulation aléatoire");
-        JList list = new JList(test); 
+        JList list_1 = new JList(test); 
+        JList list_2 = new JList(test);
         // ne comprends âs pourquoi ce truc ne marche pas ^^
-        JScrollPane scrollPane = new JScrollPane();
-        scrollPane.setViewportView(list);
+        //JScrollPane scrollPane = new JScrollPane();
+        //scrollPane.setViewportView(list_1);
         
         
         // positionnement sur le menu 
-        label.setBounds(250, 25, 300, 40);
-        list.setBounds(250, 75, 500, 200);
+        label_list1.setBounds(250, 25, 300, 40);
+        label_list2.setBounds(250, 300, 300, 40);
+        list_1.setBounds(250, 75, 500, 200);
+        list_2.setBounds(250, 350, 500, 200);
         btn_demarer_rand.setBounds(10, 100,200, 40 );
         btn_demarer_data.setBounds(10, 25, 200, 40);
         // paramétres du composant e.g taille police
         btn_demarer_rand.setFont(new Font("Arial", Font.BOLD,11));
         btn_demarer_data.setFont(new Font("Arial", Font.BOLD,11));
-        label.setFont(new Font("Arial", Font.BOLD,25));
+        label_list1.setFont(new Font("Arial", Font.BOLD,25));
+        label_list2.setFont(new Font("Arial", Font.BOLD,25));
         
         // ajout au menu 
        
         menu.add(btn_demarer_rand);
         menu.add(btn_demarer_data);
-        menu.add(label);
-        menu.add(list);
-        menu.add(scrollPane);
+        menu.add(label_list1);
+        menu.add(label_list2);
+        menu.add(list_1);
+        menu.add(list_2);
         /*
         JList list = new JList(test); 
         JButton btn_demarer_rand = new JButton("creer une simulation sans données");
@@ -89,7 +95,7 @@ public class Interfacegraph extends JFrame {
         btn_demarer_rand.addActionListener(new demarageListenner(Monde, list));
         */
         //listener 
-        btn_demarer_rand.addActionListener(new demarageListenner(Monde, list));
+        btn_demarer_rand.addActionListener(new demarageListenner(Monde, list_1));
         setDefaultCloseOperation(DISPOSE_ON_CLOSE);
     }
 
